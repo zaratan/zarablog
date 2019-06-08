d2d796f96a7042308cbfec7c2954df44dbb51788
Denis Zaratan Pasin <denis@pasin.fr>
2019-06-08 10:45:54 +0900

    No CSS plz

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = ({ siteTitle }) => (
   <header>
     <div>
-      <h1 style={{ margin: 0 }}>
+      <h1>
         <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
