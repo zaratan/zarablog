21e6859441909bfe0032ed89864844b11404b8ea
Denis Zaratan Pasin <denis@pasin.fr>
2019-06-29 22:43:23 +0900

    Update useSizeWatcher.js

--- src/hooks/useSizeWatcher.js
+++ src/hooks/useSizeWatcher.js
@@ -9,7 +9,7 @@ const genSizeWatcher = (setSize, time) =>
 
 export const useSizeWatcher = props => {
   var window;
-  const [size, setSize] = useState(typeof window === undefined ? '1440' : window.innerWidth);
+  const [size, setSize] = useState(window === undefined ? '1440' : window.innerWidth);
   useEffect(() => {
     const toWait = props === undefined ? 100 : props.wait || 100;
     const sizeWatcher = genSizeWatcher(setSize, toWait);
