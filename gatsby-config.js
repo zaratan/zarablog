329487e81bd0061c0eaae3b498899ceea7926e8d
Denis Zaratan Pasin <denis@pasin.fr>
2019-07-01 07:30:17 +0900

    Removing useless comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,7 +123,6 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: 'UA-44943460-6',
       },
     },
