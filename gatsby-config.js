220ad9c5b209575ccd84fa1d60ca5cca91e3a0df
Denis Zaratan Pasin <denis.pasin@autodesk.com>
2019-10-15 11:29:49 +0900

    Proper name for app

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Zaratan.fr`,
+        short_name: `Zaratan.fr`,
         start_url: `/`,
         background_color: `#073642`,
         theme_color: `#073642`,
